7678e015c707597efdaf59ecf73b9969fb7bfd6b
Marita Schmidt <maritaschmidt@Air-von-Marita.fritz.box>
2024-05-15 22:59:53 +0200

    happiness_index part 1 and 2

--- README.md
+++ README.md
@@ -6,3 +6,23 @@ In this repository I will collect some Jupyter notebooks with small EDA and ML p
 
 ### take_me_home_challenge_EDA.ipynb ...
 ... contains a brief analysis of a data set about students. The main focus was on finding out which group of students passed an important test and which did not. I described my approach within the notebook. 
+
+---
+
+### Setup
+
+Use the requirements file in this repo to create a new environment.
+
+```BASH
+make setup
+
+#or
+
+pyenv local 3.11.3
+python -m venv .venv
+source .venv/bin/activate
+pip install --upgrade pip
+pip install -r requirements.txt
+```
+
+The `requirements.txt` file contains the libraries needed for deployment.. of model or dashboard .. thus no jupyter or other libs used during development.
---
+++ happy_functions.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+def adjusted_r_squared(r_squared, X):
+    '''
+        function calculates the adjusted r_squared for more than one feature
+    '''
+    adjusted_r2 = 1 - ((1 - r_squared) * (len(X) - 1) / (len(X) - X.shape[1] - 1))
+    return adjusted_r2 
+
+
+def train_model(ModelClass, X_train, Y_train, **kwargs):
+    '''
+        function trains a model with Train data 
+    '''
+    model = ModelClass(**kwargs)
+    model.fit(X_train, Y_train)
+
+    return model
+
+def scatter_mae_train_test_data(df_name):
+    '''
+        function shows a seaborn scatterplot with 
+    '''
+    g = sns.scatterplot(data = df_name, x = 'model_name', y = 'test_mae', color = 'green')
+    g = sns.scatterplot(data = df_name, x = 'model_name', y = 'train_mae', marker='+', color = 'green')
+    g.set_xticklabels(g.get_xticklabels(), rotation=90)
+    g.set(title = 'mean absolute error')
+    g.legend(loc='upper right', labels=['test_mae', 'train_mae'] )
+    plt.show();
+
